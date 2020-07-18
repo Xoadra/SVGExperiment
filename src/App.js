9b27da80e8399ecba70f77af32e73d80bf0db469
Xoadra <xijp77@gmail.com>
2020-07-17 18:10:28 -0700

    Routing integrated for web page navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import logo from './logo.svg';
 import './App.css';
@@ -7,12 +8,21 @@ import './App.css';
 const App = () => {
     return (
         <div className="app">
-            <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo"/>
-                <h1>SVG Experiments</h1>
-                <img src={logo} className="app-logo" alt="logo"/>
-            </header>
-            <nav className="app-navigation"/>
+            <BrowserRouter>
+                <header className="app-header">
+                    <Link to="/" className="app-link">
+                        <img src={logo} className="app-logo" alt="logo"/>
+                        <h1>SVG Experiments</h1>
+                        <img src={logo} className="app-logo" alt="logo"/>
+                    </Link>
+                </header>
+                <nav className="app-navigation"/>
+                <Route path="/">
+                    <section className="app-section">
+                        <p>Select an SVG illustration to view.</p>
+                    </section>
+                </Route>
+            </BrowserRouter>
         </div>
     );
 };
