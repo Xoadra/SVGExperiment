418a21b9a4928d44af67e89dd8862bcfa16e32ab
Xoadra <xijp77@gmail.com>
2020-08-13 14:36:22 -0700

    Home page content moved to new Home function component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+import Home from './pages/Home';
 import Face from './pages/Face';
-import Layout from './components/Layout';
 import logo from './logo.svg';
 import './App.css';
 
 
-const App = () => {
-    return (
-        <div className="app">
-            <BrowserRouter>
-                <header className="app-header">
-                    <Link to="/">
-                        <img src={logo} className="app-logo" alt="logo"/>
-                        <h1>SVG Experiments</h1>
-                        <img src={logo} className="app-logo" alt="logo"/>
-                    </Link>
-                </header>
-                <nav className="app-navigation">
-                    <Link to="/">Home</Link>
-                    <Link to="/face">Face</Link>
-                </nav>
-                <Route path="/" exact>
-                    <Layout>
-                        <p>Select an SVG illustration to view.</p>
-                    </Layout>
-                </Route>
-                <Route path="/face" component={Face}/>
-            </BrowserRouter>
-        </div>
-    );
-};
+const App = () => (
+    <BrowserRouter>
+        <header className="app-header">
+            <Link to="/">
+                <img src={logo} className="app-logo" alt="logo"/>
+                <h1>SVG Experiments</h1>
+                <img src={logo} className="app-logo" alt="logo"/>
+            </Link>
+        </header>
+        <nav className="app-navigation">
+            <Link to="/">Home</Link>
+            <Link to="/face">Face</Link>
+        </nav>
+        <Route exact path="/" component={Home}/>
+        <Route path="/face" component={Face}/>
+    </BrowserRouter>
+);
 
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+
+const Home = () => (
+    <Layout>
+        <p>Select an SVG illustration to view.</p>
+    </Layout>
+);
+
+
+export default Home;
